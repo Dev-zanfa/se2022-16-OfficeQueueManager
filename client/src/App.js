5e48c6d7e496a76df1056d58c569dc0d82215bdb
DAVIDE ANFOSSI s301201 <s301201@studenti.polito.it>
2022-10-18 15:37:40 +0200

    S202216OQM-13  added customer gui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
-import { LoginForm } from "./Components/LoginForm";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import { LoginForm } from "./Components/LoginForm";
+import { Customer } from "./Components/Customer";
+
 function App() {
-  return <>
-  <LoginForm></LoginForm>
-  </>;
+  return <Router>
+    <Main/>
+  </Router>;
+}
+
+function Main(){
+
+  return <Routes>   
+    <Route path="/">
+      <Route path=""/>  
+      <Route path="login" element={<LoginForm/>}/> 
+      <Route path="customer" element={<Customer/>}/> 
+    </Route>
+  </Routes>;
 }
 
 export default App;
