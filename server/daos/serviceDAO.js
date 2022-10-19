9dab51dcc463a369f5cca4bdbdf79f161c05188b
riccardogiambra <100277759+riccardogiambra@users.noreply.github.com>
2022-10-19 17:44:13 +0200

    S202216OQM-15 added service management and bug fix

--- server/daos/serviceDAO.js
+++ server/daos/serviceDAO.js
@@ -1,25 +1,33 @@
-'use strict';
+"use strict";
 
-const Service = require('../dtos/serviceDTO');
+const Service = require("../dtos/serviceDTO");
 
 class ServiceDAO {
+  constructor(dbManager) {
+    if (!dbManager) throw "DBManager must be defined for Service dao!";
+    this.dbManager = dbManager;
+  }
 
-    constructor(dbManager) {
-        if (!dbManager)
-            throw 'DBManager must be defined for Service dao!';
-        this.dbManager = dbManager;
+  async getService(serviceTag) {
+    const query = "SELECT * FROM Service WHERE tag = ?";
+    try {
+      const result = await this.dbManager.get(query, [serviceTag], true);
+      return new Service(result.tag, result.name, result.service_time);
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async getService(serviceTag) {
-        const query = "SELECT * FROM Service WHERE tag = ?";
-        try {
-            const result = await this.dbManager.get(query, [serviceTag], true);
-            return new Service(result.tag, result.name, result.service_time);
-        } catch (err) {
-            throw err;
-        }
+  async getAllServices() {
+    const query = "SELECT tag FROM Service";
+    try {
+      const result = await this.dbManager.get(query);
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw err;
     }
-    
+  }
 }
 
 module.exports = ServiceDAO;
--- server/services/ticketService.js
+++ server/services/ticketService.js
@@ -1,39 +1,44 @@
-'use strict'
-
+"use strict";
 
 class TicketService {
-    constructor(ticketDAO, queueDAO) {
-        if (!ticketDAO)
-            throw 'ticketDAO must be defined for ticketService service!';
-        else if(!queueDAO)
-            throw 'queueDAO must be defined for ticketService service!';
-        this.queueDAO=queueDAO;
-        this.ticketDAO = ticketDAO;
+  constructor(ticketDAO, serviceDao, queueDao) {
+    if (!ticketDAO)
+      throw "ticketDAO must be defined for ticketService service!";
+    if (!serviceDao)
+      throw "serviceDao must be defined for ticketService service!";
+    if (!queueDao) throw "queueDao must be defined for ticketService service!";
+
+    this.ticketDAO = ticketDAO;
+    this.serviceDao = serviceDao;
+    this.queueDao = queueDao;
+  }
+
+  async addTicket(service) {
+    try {
+      let services = await this.serviceDao.getAllServices();
+      //let services = ['a', 'b', 'c', 'd'];
+      if (!service || !services.map((elem) => elem.tag).includes(service)) {
+        throw {
+          returnCode: 422,
+          message: "validation of request body failed, service does not exists",
+        };
+      }
+      const areThereAnyTicket = await this.ticketDAO.countTicketPerService(
+        service
+      );
+      const lastNumberforservice = areThereAnyTicket
+        ? await this.ticketDAO.getLastTicketPerService(service)
+        : (await this.queueDao.getQueueByService(service)).count;
+      let newNumberforservice = lastNumberforservice + 1;
+      const response = await this.ticketDAO.insertTicket(
+        service,
+        newNumberforservice
+      );
+      return response;
+    } catch (err) {
+      throw err;
     }
-
-    async addTicket(service) {
-        try {
-            //let services = retrieve from database??
-            let services = ['a', 'b', 'c', 'd'];
-            if (!service || !services.includes(service)) {
-                throw {
-                    returnCode: 422,
-                    message: 'validation of request body failed, service does not exists'
-                };
-            }
-            let newNumberforservice=0;
-            const lastNumberforservice = await this.ticketDAO.getLastTicketPerService(service);
-            const queueLen = await this.queueDAO.getQueueByService(service);
-
-            (lastNumberforservice==0 && queueLen!=0) ? newNumberforservice = queueLen +1  : newNumberforservice = lastNumberforservice + 1 ;
-            
-            const response = await this.ticketDAO.insertTicket(service, newNumberforservice);
-            return response;
-        } catch (err) {
-            throw err;
-        }
-    }
-
+  }
 }
 
 module.exports = TicketService;
