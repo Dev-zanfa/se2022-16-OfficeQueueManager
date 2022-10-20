1f5ee3d73acacff57fcf1766545b937e30b32749
Marziyeh Karani s301356 <s301356@studenti.polito.it>
2022-10-20 11:57:01 +0200

    S202216OQM-12 refactored code

--- client/src/Components/Counter.js
+++ client/src/Components/Counter.js
@@ -9,7 +9,7 @@ function Counter(props) {
     <Header></Header>
     <Row className='justify-content-center align-items-center w-100 vh-100'>
         <Row className='justify-content-center'> 
-            <Button style={{width: "200px", height: "100px", marginTop: "20px"}} onClick={() => {API.nextCustomer();}}>
+            <Button style={{width: "200px", height: "100px", marginTop: "20px"}} onClick={() => {props.nextCustomer();}}>
                     <div>Next customer</div>
             </Button>
         </Row>
